d3b210d46f32f7637fb407eb09a3c79432ffe26f
pablito0412 <pablo200.a@gmail.com>
2025-04-22 10:40:11 -0500

    backend y fronted

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+// Importar Express
+const express = require('express');
+const cors = require('cors');
+
+// Inicializar la aplicación Express
+const server = express();
+server.use(cors());
+
+// Definir el puerto para el servidor
+const PORT = 3000;
+
+server.get('/ping', (req, res) => {
+    res.json({message: 'pong-Chulde' })
+});
+
+// Iniciar el servidor
+server.listen(PORT, () => {
+    console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
+});
